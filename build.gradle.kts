7c631169d5f3cac2cbc1cdd1715d5936e8ce95a1
Marcos Andrade <marcos_q_andrade@hotmail.com>
2024-10-18 11:06:48 -0300

    creat project

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+plugins {
+    kotlin("jvm") version "2.0.10"
+    kotlin("plugin.allopen") version "2.0.10"
+    id("io.quarkus")
+}
+
+repositories {
+    mavenCentral()
+    mavenLocal()
+}
+
+val quarkusPlatformGroupId: String by project
+val quarkusPlatformArtifactId: String by project
+val quarkusPlatformVersion: String by project
+
+dependencies {
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:${quarkusPlatformArtifactId}:${quarkusPlatformVersion}"))
+    implementation(enforcedPlatform("${quarkusPlatformGroupId}:quarkus-amazon-services-bom:${quarkusPlatformVersion}"))
+    implementation("io.quarkus:quarkus-kafka-client")
+    implementation("io.quarkus:quarkus-resteasy")
+    implementation("io.quarkiverse.amazonservices:quarkus-amazon-sns")
+    implementation("io.quarkus:quarkus-smallrye-jwt")
+    implementation("io.quarkiverse.amazonservices:quarkus-amazon-s3")
+    implementation("io.quarkus:quarkus-jdbc-mysql")
+    implementation("io.quarkiverse.amazonservices:quarkus-amazon-sqs")
+    implementation("io.quarkus:quarkus-hibernate-validator")
+    implementation("io.quarkus:quarkus-smallrye-openapi")
+    implementation("io.quarkus:quarkus-kotlin")
+    implementation("io.quarkus:quarkus-hibernate-orm-panache")
+    implementation("io.quarkus:quarkus-resteasy-jackson")
+    implementation("org.jetbrains.kotlin:kotlin-stdlib-jdk8")
+    implementation("io.quarkus:quarkus-arc")
+    implementation("io.quarkus:quarkus-hibernate-orm")
+    testImplementation("io.quarkus:quarkus-junit5")
+    testImplementation("io.rest-assured:rest-assured")
+}
+
+group = "com.digitalwallet"
+version = "1.0-SNAPSHOT"
+
+java {
+    sourceCompatibility = JavaVersion.VERSION_22
+    targetCompatibility = JavaVersion.VERSION_22
+}
+
+tasks.withType<Test> {
+    systemProperty("java.util.logging.manager", "org.jboss.logmanager.LogManager")
+}
+allOpen {
+    annotation("jakarta.ws.rs.Path")
+    annotation("jakarta.enterprise.context.ApplicationScoped")
+    annotation("jakarta.persistence.Entity")
+    annotation("io.quarkus.test.junit.QuarkusTest")
+}
+
+tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinCompile> {
+    compilerOptions {
+        jvmTarget.set(org.jetbrains.kotlin.gradle.dsl.JvmTarget.JVM_22)
+        freeCompilerArgs.add("-Xjava-parameters")
+    }
+}
+
---
+++ src/native-test/kotlin/com/digitalwallet/ExampleResourceIT.kt
@@ -0,0 +1,6 @@
+package com.digitalwallet
+
+import io.quarkus.test.junit.QuarkusIntegrationTest
+
+@QuarkusIntegrationTest
+class ExampleResourceIT : ExampleResourceTest()
